c9eb0cde6b1cb981201b6d2c388a7b24bddf5e24
Hien Luu <hluu@linkedin.com>
2020-03-14 18:26:00 -0700

    added bubble sort

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,5 +1,71 @@
+import org.testng.Assert;
+
+import java.util.Arrays;
+
+/**
+ * Bubble sorting:
+ * - bubble either
+ *   - large value to the right
+ *   - small value to the left
+ */
 public class BubbleSort {
     public static void main(String[] args) {
         System.out.println("BubbleSort.main");
+
+        int[] tmpArr = SortingUtil.generate(20, 50);
+
+        test(tmpArr);
     }
+
+    private static void test(int[] arr) {
+        System.out.println("input:       " + Arrays.toString(arr));
+
+
+        int[] bubbleLargeArr = Arrays.copyOf(arr, arr.length);
+        int[] bubbleSmallArr = Arrays.copyOf(arr, arr.length);
+
+        Arrays.sort(arr);
+
+        bubbleLarge(bubbleLargeArr);
+        bubbleSmall(bubbleSmallArr);
+
+        System.out.println("arr:         " + Arrays.toString(arr));
+        System.out.println("bubbleLarge: " + Arrays.toString(bubbleLargeArr));
+        System.out.println("bubbleSmall: " + Arrays.toString(bubbleSmallArr));
+
+        Assert.assertTrue(SortingUtil.isTheSame(arr, bubbleLargeArr));
+        Assert.assertTrue(SortingUtil.isTheSame(arr, bubbleSmallArr));
+    }
+
+    /**
+     * Bubble large values front to back
+     *
+     * @param arr
+     */
+    public static void bubbleLarge(int[] arr) {
+        for (int right = arr.length-1; right > 1; right--) {
+
+            for (int left = 0; left < right; left++) {
+                if (arr[left] > arr[left+1]) {
+                    SortingUtil.swap(arr, left, left+1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Bubble the small values from back to front
+     * @param arr
+     */
+    public static void bubbleSmall(int[] arr) {
+        for (int left = 0; left < arr.length; left++) {
+
+            for (int right = arr.length-1; right > left; right--) {
+                if (arr[right] < arr[right-1]) {
+                    SortingUtil.swap(arr, right, right-1);
+                }
+            }
+        }
+    }
+    
 }
---
+++ src/main/java/SortingUtil.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class SortingUtil {
+    public static int[] generate(int size, int bound) {
+        Random rand = new Random(33);
+
+        int[] tmpArr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            tmpArr[i] = rand.nextInt(bound);
+        }
+
+        return tmpArr;
+    }
+
+    public static void swap(int[] arr, int left, int right) {
+        int tmp = arr[left];
+
+        arr[left] = arr[right];
+        arr[right] = tmp;
+    }
+
+    public static boolean isTheSame(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null) {
+            return false;
+        }
+
+        if (arr1.length != arr2.length) {
+            return  false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
